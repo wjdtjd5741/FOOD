9b127e251ba7121e637c322a06fea1e6a201aefd
이기영 <yi5oyu@naver.com>
2023-09-21 16:58:17 +0900

    mypage sec0  div -> textarea, 수정 이벤트 추가

--- food_project/assets/js/btns_hover.js
+++ food_project/assets/js/btns_hover.js
@@ -2,7 +2,8 @@ btns_hover()
 
 function btns_hover(){
         let btns = document.querySelectorAll(".btns_hover")
-        // console.log(btns[0].style.color)
+        let sec0_text = document.querySelectorAll(".text_d")
+        console.log(sec0_text)
         for(let i = 0 ;i<btns.length ;i++){
             btns[i].addEventListener("mouseover",function(){
                     btns[i].style.color = "white"
@@ -11,6 +12,15 @@ function btns_hover(){
             btns[i].addEventListener("mouseout",function(){
                     btns[i].style.color = "black"
                     btns[i].style.backgroundColor = "rgba(207, 207, 207, 0.562)"
+            })
+            btns[i].addEventListener("click",function(){
+                if(btns[i].innerText == "수정"){
+                        sec0_text[i].disabled = false;
+                        btns[i].innerText = "저장"
+                    } else{
+                        sec0_text[i].disabled = true;
+                        btns[i].innerText = "수정"
+                    }
             })
         }
 }
